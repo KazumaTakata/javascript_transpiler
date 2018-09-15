5cd0d87cf6dad3849ae50e78b6b7f8055ce4b073
Kazuma Takata <neilbuzztakata@gmail.com>
2018-09-15 15:38:45 +0900

    add test

--- test/lexer_test.js
+++ test/lexer_test.js
@@ -4,6 +4,13 @@ const evaluator = require("../evaluate/evaluate");
 // let input = "1 + 1 + 3";
 // let input = " 1 + (3+4) ";
 // let input = " 1 + (4 +  4 ) + 5";
+let input = "let x = 1";
+let input = " x == 1 ";
+let input = `
+    if ( true ){
+        x = 1
+    }
+`;
 
 let tokens = Lexer.lex(input);
 
